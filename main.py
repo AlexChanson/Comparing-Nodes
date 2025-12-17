3327be9e574038a8f00288b7a57c984dbc7701d6
Alexandre Chanson <alexandre.chanson@univ-tours.fr>
2025-12-17 11:04:44 +0100

    fully integrated Cpp bakcend for mincut solver

--- main.py
+++ main.py
@@ -377,7 +377,7 @@ if __name__ == '__main__':
             for i in range(len(data)):
                 f.write(",".join(map(str, data[i])) + "\n")
 
-        result = subprocess.run([binary_path, "--dataset", "/tmp/cmp_nodes_temp.csv", "--k", args.k],capture_output=True,text=True)
+        result = subprocess.run([binary_path, "--k", str(k), "--dataset", "/tmp/cmp_nodes_temp.csv"],capture_output=True,text=True)
 
         if result.returncode == 0:
             # Get output as a list of strings
@@ -398,7 +398,6 @@ if __name__ == '__main__':
             sol.membership = membership
             sol.sol = mask
             sol.obj = obj
-            #print(lines)
             print("[best solution]:", sol)
             print("[Silhouette]", silhouette_score(data[:, sol.derive_clustering_mask()], sol.membership))
         else:
